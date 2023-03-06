aba49420b46bfb75eef30a21aeaeff800bd44431
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-06 17:43:09 -0300

    Test

--- Telas/src/manipulatelas/ManipulaTelas.java
+++ Telas/src/manipulatelas/ManipulaTelas.java
@@ -2,8 +2,10 @@ package manipulatelas;
 
 import telas.FirstScreen;
 
+import javax.swing.*;
+
 public class ManipulaTelas {
     public static void main(String[] args) {
-        new FirstScreen();
+
     }
 }
