689556c593e9036d387c36822563364e7bba7cf1
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-07 13:16:48 -0300

    Parse JSON to String

--- Telas/src/moedas/JsonParser.java
+++ Telas/src/moedas/JsonParser.java
@@ -1,2 +1,14 @@
-package moedas;public class JsonParser {
+package moedas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class JsonParser {
+    public static String jsonEmString(BufferedReader buffereReader) throws IOException{
+        String resposta, jsonEmString = "";
+        while((resposta = buffereReader.readLine()) != null){
+            jsonEmString += resposta;
+        }
+        return jsonEmString;
+    }
 }
