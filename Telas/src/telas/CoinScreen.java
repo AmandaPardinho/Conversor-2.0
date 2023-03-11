19ad404ca29c07f1ce4503571f930d824f4b37f9
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-11 16:16:32 -0300

    Creating the coin array

--- Telas/src/telas/CoinScreen.java
+++ Telas/src/telas/CoinScreen.java
@@ -1,2 +1,8 @@
-package telas;public class CoinScreen {
+package telas;
+
+public class CoinScreen {
+    //Definição das opções de moedas do ComboBox
+    private String[] opcaoMoedas = {"Selecione uma moeda", "Dólar (USD)", "Euro (EUR)", "Real (BRL)",
+            "Peso Argentino (ARS)","Libra Esterlina (GBP)", "Yuan Chinês (CNY)"};
+
 }
