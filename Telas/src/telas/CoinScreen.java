3a7ed8588f97737ee79c0645d133b12f6a010505
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-11 16:45:02 -0300

    Changing GenericScreen constructor parameters according to the CoinScreen class

--- Telas/src/telas/CoinScreen.java
+++ Telas/src/telas/CoinScreen.java
@@ -1,8 +1,13 @@
 package telas;
 
+import javax.swing.*;
+
 public class CoinScreen {
     //Definição das opções de moedas do ComboBox
     private String[] opcaoMoedas = {"Selecione uma moeda", "Dólar (USD)", "Euro (EUR)", "Real (BRL)",
             "Peso Argentino (ARS)","Libra Esterlina (GBP)", "Yuan Chinês (CNY)"};
+
+    GenericScreen coinScreen = new GenericScreen("Conversor de Moedas", "Moedas",
+            "Valor a converter", opcaoMoedas, opcaoMoedas);
 
 }
