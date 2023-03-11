79f8fe2b2db7f59ad45eff0004f3d29428d36a75
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-10 21:04:00 -0300

    Few tests using main class

--- Telas/src/manipulatelas/ManipulaTelas.java
+++ Telas/src/manipulatelas/ManipulaTelas.java
@@ -1,5 +1,7 @@
 package manipulatelas;
 
+import moedas.Dolar;
+import moedas.Moeda;
 import telas.GenericScreen;
 import telas.FirstScreen;
 
@@ -7,8 +9,16 @@ import javax.swing.*;
 
 public class ManipulaTelas {
     public static void main(String[] args) {
-        new FirstScreen();
-        new GenericScreen();
+ //       new FirstScreen();
+//        new GenericScreen();
+
+       GenericScreen telas = new GenericScreen();
+       telas.fecharTela();
+//        Dolar dolar = new Dolar();
+//        Moeda moeda = new Moeda();
+
+//        dolar.setRealDolar("BRLUSD");
+//        dolar.setDolarReal("USDBRL");
 
 
     }
