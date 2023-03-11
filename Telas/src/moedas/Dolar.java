e4d92b9ba3190035fd6273fca127c7d320d096ce
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-10 21:03:15 -0300

    Started a new java class

--- Telas/src/moedas/Dolar.java
+++ Telas/src/moedas/Dolar.java
@@ -1,2 +1,22 @@
-package moedas;public class Dolar {
+package moedas;
+
+public class Dolar {
+    private String dolarReal;
+    private String realDolar;
+
+    public String getDolarReal() {
+        return dolarReal;
+    }
+
+    public void setDolarReal(String dolarReal) {
+        this.dolarReal = dolarReal;
+    }
+
+    public String getRealDolar() {
+        return realDolar;
+    }
+
+    public void setRealDolar(String realDolar) {
+        this.realDolar = realDolar;
+    }
 }
