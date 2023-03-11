9ed697b140499e8465c31c8c63a0ce3c60e7b1d8
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-03-11 16:45:37 -0300

    Commented code

--- Telas/src/manipulatelas/ManipulaTelas.java
+++ Telas/src/manipulatelas/ManipulaTelas.java
@@ -1,19 +1,14 @@
 package manipulatelas;
 
-import moedas.Dolar;
-import moedas.Moeda;
 import telas.GenericScreen;
-import telas.FirstScreen;
-
-import javax.swing.*;
 
 public class ManipulaTelas {
     public static void main(String[] args) {
  //       new FirstScreen();
 //        new GenericScreen();
 
-       GenericScreen telas = new GenericScreen();
-       telas.fecharTela();
+       //GenericScreen telas = new GenericScreen();
+       //telas.fecharTela();
 //        Dolar dolar = new Dolar();
 //        Moeda moeda = new Moeda();
 
